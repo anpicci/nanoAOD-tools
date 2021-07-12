40c87aeb94c41edc26619904016b89281cc8e3c0
apiccine <andrea.piccinelli@cern.ch>
2021-07-12 15:40:38 +0200

    make makeplot fit to emu channel

--- python/postprocessing/makeplot.py
+++ python/postprocessing/makeplot.py
@@ -115,9 +115,9 @@ if opt.bveto:
         cut_tag = cut_tag+ '_AND_' + cutToTag(opt.cut) 
 
 if opt.bbv:
-    cut_dict = {'muon':"(abs(" + mpdgstr + "_pdgid)==13&&pass_lepton_selection==1&&pass_lepton_veto==1&&pass_tau_selection==1&&pass_charge_selection==1&&pass_jet_selection==1)*(" + cut + ")", 
-                 'electron':"(abs(" + epdgstr + "_pdgid)==11&&pass_lepton_selection==1&&pass_lepton_veto==1&&pass_tau_selection==1&&pass_charge_selection==1&&pass_jet_selection==1)*(" + cut + ")", 
-                 'incl':"((abs(" + mpdgstr + "_pdgid)==13" + incl_logic + "abs(" + epdgstr + "_pdgid)==11)&&pass_lepton_selection==1&&pass_lepton_veto==1&&pass_charge_selection==1&&pass_jet_selection==1&&pass_tau_veto==1)*(" + cut + ")", 
+    cut_dict = {'muon':"(abs(" + mpdgstr + "_pdgid)==13&&pass_lepton_selection==1&&pass_tau_selection==1&&pass_lepton_veto==1&&pass_charge_selection==0&&pass_jet_selection==1&&MET_pt>50.)*(" + cut + ")", 
+                 'electron':"(abs(" + epdgstr + "_pdgid)==11&&pass_lepton_selection==1&&pass_tau_selection==1&&pass_lepton_veto==1&&pass_charge_selection==0&&pass_jet_selection==1&&MET_pt>50.)*(" + cut + ")", 
+                 'incl':"((abs(" + mpdgstr + "_pdgid)==13" + incl_logic + "abs(" + epdgstr + "_pdgid)==11)&&)*(" + cut + "pass_lepton_selection==1&&pass_lepton_veto==0&&pass_charge_selection==0&&pass_jet_selection==1&&pass_tau_veto==1&&MET_pt>50.)", 
     }
     cut_tag = 'selection_before_bveto'
     if opt.cut != "1.":
