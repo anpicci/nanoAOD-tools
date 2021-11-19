7b41b7164f8fe3f83cf3aa12bbb5c9e784b703aa
apiccine <andrea.piccinelli@cern.ch>
2021-11-19 08:37:29 +0100

    UL crab update

--- crab/submit_crab.py
+++ crab/submit_crab.py
@@ -52,6 +52,12 @@ def cfg_writer(sample, isMC, outdir):
             f.write("config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/ReReco/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON_v1.txt'\n")
         elif sample.year == '2018':
             f.write("config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/ReReco/Cert_314472-325175_13TeV_17SeptEarlyReReco2018ABC_PromptEraD_Collisions18_JSON.txt'\n")
+        elif sample.year == 'UL2016':
+            f.write("config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Legacy_2016/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt'\n")
+        elif sample.year == 'UL2017':
+            f.write("config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/Legacy_2017/Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt'\n")
+        elif sample.year == 'UL2018':
+            f.write("config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/Legacy_2018/Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt'\n")
         if not ("DataHT" in sample.name and sample.year=='2018'):
             f.write("config.Data.unitsPerJob = 50\n")
         else:
--- python/postprocessing/examples/MET_HLT_Filter.py
+++ python/postprocessing/examples/MET_HLT_Filter.py
@@ -6,7 +6,7 @@ from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
 
 class MET_HLT_Filter(Module):
     def __init__(self, year):
-        self.year = year
+        self.year = str(year)
         pass
     def endJob(self):
         pass
@@ -17,18 +17,27 @@ class MET_HLT_Filter(Module):
         HLT = Object(event, "HLT")
         L1 = Object(event, "L1")
         flag = Object(event, 'Flag')
-        good_MET = flag.goodVertices and flag.HBHENoiseFilter and flag.HBHENoiseIsoFilter and flag.EcalDeadCellTriggerPrimitiveFilter and flag.BadPFMuonFilter
-        
-        if(self.year == 2016):
+        if not "UL" in year:
+            good_MET = flag.goodVertices and flag.HBHENoiseFilter and flag.HBHENoiseIsoFilter and flag.EcalDeadCellTriggerPrimitiveFilter and flag.BadPFMuonFilter
+        else:
+            if "2017" in year or "2018" in year:
+                good_MET = flag.goodVertices and flag.globalSuperTightHalo2016Filter and flag.HBHENoiseFilter and flag.HBHENoiseIsoFilter and flag.EcalDeadCellTriggerPrimitiveFilter and flag.BadPFMuonFilter and flag.BadPFMuonDzFilter and flag.eeBadScFilter and flag.ecalBadCalibFilter
+            elif "2016" in year:
+                good_MET = flag.goodVertices and flag.globalSuperTightHalo2016Filter and flag.HBHENoiseFilter and flag.HBHENoiseIsoFilter and flag.EcalDeadCellTriggerPrimitiveFilter and flag.BadPFMuonFilter and flag.BadPFMuonDzFilter and flag.eeBadScFilter
+        if(self.year == "2016"):
             good_HLT = (HLT.Ele27_WPTight_Gsf or HLT.Ele32_WPTight_Gsf or HLT.IsoMu24 or HLT.IsoTkMu24) and flag.globalSuperTightHalo2016Filter
-        elif(self.year == 2017):
+        elif(self.year == "2017"):
             good_HLT = (HLT.IsoMu27 or HLT.Mu50 or HLT.Ele35_WPTight_Gsf or HLT.Ele32_WPTight_Gsf_L1DoubleEG or HLT.Photon200)# or HLT.PFHT250 or HLT.PFHT350)
-        elif(self.year == 2018):
+        elif(self.year == "2018"):
             good_HLT = (HLT.IsoMu27 or HLT.Mu50 or HLT.Ele35_WPTight_Gsf or HLT.Ele32_WPTight_Gsf_L1DoubleEG or HLT.Photon200)# or HLT.PFHT250 or HLT.PFHT350)
         else:
             print("Please specify the year: possible choices are 2016, 2017 or 2018")
         return good_MET and good_HLT
 
-MET_HLT_Filter_2016 = lambda : MET_HLT_Filter(2016)
-MET_HLT_Filter_2017 = lambda : MET_HLT_Filter(2017)
-MET_HLT_Filter_2018 = lambda : MET_HLT_Filter(2018)
+MET_HLT_Filter_2016 = lambda : MET_HLT_Filter("2016")
+MET_HLT_Filter_2017 = lambda : MET_HLT_Filter("2017")
+MET_HLT_Filter_2018 = lambda : MET_HLT_Filter("2018")
+MET_HLT_Filter_UL2016APV = lambda : MET_HLT_Filter("UL2016APV")
+MET_HLT_Filter_UL2016 = lambda : MET_HLT_Filter("UL2016")
+MET_HLT_Filter_UL2017 = lambda : MET_HLT_Filter("UL2017")
+MET_HLT_Filter_UL2018 = lambda : MET_HLT_Filter("UL2018")
