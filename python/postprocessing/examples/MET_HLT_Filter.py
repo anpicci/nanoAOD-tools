e20cd94e0f9da373fec54366e8a4ec40bde67b3b
apiccine <andrea.piccinelli@cern.ch>
2021-11-22 20:49:20 +0100

    UL HLT menu added in crab macro

--- python/postprocessing/examples/MET_HLT_Filter.py
+++ python/postprocessing/examples/MET_HLT_Filter.py
@@ -19,20 +19,33 @@ class MET_HLT_Filter(Module):
         flag = Object(event, 'Flag')
         if not "UL" in year:
             good_MET = flag.goodVertices and flag.HBHENoiseFilter and flag.HBHENoiseIsoFilter and flag.EcalDeadCellTriggerPrimitiveFilter and flag.BadPFMuonFilter
+            if(self.year == "2016"):
+                good_HLT = (HLT.Ele27_WPTight_Gsf or HLT.Ele32_WPTight_Gsf or HLT.IsoMu24 or HLT.IsoTkMu24) and flag.globalSuperTightHalo2016Filter
+            elif(self.year == "2017"):
+                good_HLT = (HLT.IsoMu27 or HLT.Mu50 or HLT.Ele35_WPTight_Gsf or HLT.Ele32_WPTight_Gsf_L1DoubleEG or HLT.Photon200)# or HLT.PFHT250 or HLT.PFHT350)
+            elif(self.year == "2018"):
+                good_HLT = (HLT.IsoMu27 or HLT.Mu50 or HLT.Ele35_WPTight_Gsf or HLT.Ele32_WPTight_Gsf_L1DoubleEG or HLT.Photon200)# or HLT.PFHT250 or HLT.PFHT350)
+            else:
+                print("Please specify the year: possible choices are 2016, 2017 or 2018")
+
         else:
             if "2017" in year or "2018" in year:
                 good_MET = flag.goodVertices and flag.globalSuperTightHalo2016Filter and flag.HBHENoiseFilter and flag.HBHENoiseIsoFilter and flag.EcalDeadCellTriggerPrimitiveFilter and flag.BadPFMuonFilter and flag.BadPFMuonDzFilter and flag.eeBadScFilter and flag.ecalBadCalibFilter
             elif "2016" in year:
                 good_MET = flag.goodVertices and flag.globalSuperTightHalo2016Filter and flag.HBHENoiseFilter and flag.HBHENoiseIsoFilter and flag.EcalDeadCellTriggerPrimitiveFilter and flag.BadPFMuonFilter and flag.BadPFMuonDzFilter and flag.eeBadScFilter
-        if(self.year == "2016"):
-            good_HLT = (HLT.Ele27_WPTight_Gsf or HLT.Ele32_WPTight_Gsf or HLT.IsoMu24 or HLT.IsoTkMu24) and flag.globalSuperTightHalo2016Filter
-        elif(self.year == "2017"):
-            good_HLT = (HLT.IsoMu27 or HLT.Mu50 or HLT.Ele35_WPTight_Gsf or HLT.Ele32_WPTight_Gsf_L1DoubleEG or HLT.Photon200)# or HLT.PFHT250 or HLT.PFHT350)
-        elif(self.year == "2018"):
-            good_HLT = (HLT.IsoMu27 or HLT.Mu50 or HLT.Ele35_WPTight_Gsf or HLT.Ele32_WPTight_Gsf_L1DoubleEG or HLT.Photon200)# or HLT.PFHT250 or HLT.PFHT350)
-        else:
-            print("Please specify the year: possible choices are 2016, 2017 or 2018")
-        return good_MET and good_HLT
+        
+            if(self.year == "UL2016APV" or self.year == "UL2016"):
+                good_HLT = (HLT.IsoMu24 or HLT.IsoTkMu24 or HLT.Mu50 or HLT.TkMu50 or HLT.Ele27_WPTight_Gsf or HLT.Ele32_WPTight_Gsf or HLT.Photon175)
+            elif(self.year == "UL2017"):
+                good_HLT = (HLT.IsoMu27 or HLT.Mu50 or HLT.OldMu100 or HLT.TkMu100 or HLT.Ele35_WPTight_Gsf or (HLT.Ele32_WPTight_Gsf_L1DoubleEG and (L1.SingleIsoEG30er2p1 or L1.SingleIsoEG32 or L1.SingleEG40)) or HLT.Photon200)# or HLT.PFHT250 or HLT.PFHT350)
+            elif(self.year == "UL2018"):
+                good_HLT = (HLT.IsoMu24 or HLT.Mu50 or HLT.OldMu100 or HLT.TkMu100 or HLT.Ele32_WPTight_Gsf or HLT.Photon200)# or HLT.PFHT250 or HLT.PFHT350)
+            else:
+                print("Please specify the year: possible choices are 2016, 2017 or 2018")
+
+        good_evt = (good_MET and good_HLT)
+        return good_evt
+        
 
 MET_HLT_Filter_2016 = lambda : MET_HLT_Filter("2016")
 MET_HLT_Filter_2017 = lambda : MET_HLT_Filter("2017")
