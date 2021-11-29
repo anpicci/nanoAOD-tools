5c813327486411341630b4fd6904e9b184e5e1e5
apiccine <andrea.piccinelli@cern.ch>
2021-11-29 16:15:46 +0100

    MET_HLT_fake update for UL

--- python/postprocessing/examples/MET_HLT_Filter_Fake.py
+++ python/postprocessing/examples/MET_HLT_Filter_Fake.py
@@ -19,36 +19,53 @@ class MET_HLT_Filter_Fake(Module):
         L1 = Object(event, "L1")
         flag = Object(event, 'Flag')
         good_MET = flag.goodVertices and flag.HBHENoiseFilter and flag.HBHENoiseIsoFilter and flag.EcalDeadCellTriggerPrimitiveFilter and flag.BadPFMuonFilter
+
+        if not "UL" in self.year:
+            if("2016" in self.year):
+                if self.trig == "HT":
+                    good_HLT = HLT.PFHT250 or HLT.PFHT350 or HLT.PFHT370 or HLT.PFHT430 or HLT.PFHT510 or HLT.PFHT590 or HLT.PFHT680 or HLT.PFHT780 or HLT.PFHT890
+                    #good_HLT = (HLT.Ele27_WPTight_Gsf or HLT.Ele32_WPTight_Gsf or HLT.IsoMu24 or HLT.IsoTkMu24) and flag.globalSuperTightHalo2016Filter
+            elif("2017" in self.year):
+                #if self.trig == "Lep":
+                #good_HLT = HLT.Ele8_CaloIdL_TrackIdL_IsoVL_PFJet30 or HLT.Ele12_CaloIdL_TrackIdL_IsoVL_PFJet30 or HLT.Ele23_CaloIdL_TrackIdL_IsoVL_PFJet30 or HLT.Mu8_TrkIsoVVL or HLT.Mu17_TrkIsoVVL or HLT.Mu15_IsoVVVL_PFHT600
+                #elif self.trig == "Tau":
+                #good_HLT = HLT.IsoMu20_eta2p1_LooseChargedIsoPFTau27_eta2p1_CrossL1 or HLT.Ele24_eta2p1_WPTight_Gsf_LooseChargedIsoPFTau30_eta2p1_CrossL1 or HLT.MediumChargedIsoPFTau50_Trk30_eta2p1_1pr
+                if self.trig == "HT":
+                    good_HLT = HLT.PFHT250 or HLT.PFHT350 or HLT.PFHT370 or HLT.PFHT430 or HLT.PFHT510 or HLT.PFHT590 or HLT.PFHT680 or HLT.PFHT780 or HLT.PFHT890
+
+            elif("2018" in self.year):
+                #if self.trig == "Lep":
+                #good_HLT = HLT.Ele8_CaloIdL_TrackIdL_IsoVL_PFJet30 or HLT.Ele12_CaloIdL_TrackIdL_IsoVL_PFJet30 or HLT.Ele23_CaloIdL_TrackIdL_IsoVL_PFJet30 or HLT.Mu8_TrkIsoVVL or HLT.Mu17_TrkIsoVVL or HLT.Mu15_IsoVVVL_PFHT600
+                #elif self.trig == "Tau":
+                #good_HLT = HLT.IsoMu20_eta2p1_LooseChargedIsoPFTau27_eta2p1_CrossL1 or HLT.Ele24_eta2p1_WPTight_Gsf_LooseChargedIsoPFTau30_eta2p1_CrossL1 or HLT.MediumChargedIsoPFTau50_Trk30_eta2p1_1pr
+                if self.trig == "HT":
+                    good_HLT = HLT.PFHT250 or HLT.PFHT350 or HLT.PFHT370 or HLT.PFHT430 or HLT.PFHT510 or HLT.PFHT590 or HLT.PFHT680 or HLT.PFHT780 or HLT.PFHT890
         
-        if("2016" in self.year):
-            if self.trig == "HT":
-                good_HLT = HLT.PFHT250 or HLT.PFHT350 or HLT.PFHT370 or HLT.PFHT430 or HLT.PFHT510 or HLT.PFHT590 or HLT.PFHT680 or HLT.PFHT780 or HLT.PFHT890
-            #good_HLT = (HLT.Ele27_WPTight_Gsf or HLT.Ele32_WPTight_Gsf or HLT.IsoMu24 or HLT.IsoTkMu24) and flag.globalSuperTightHalo2016Filter
-        elif("2017" in self.year):
-            #if self.trig == "Lep":
-                #good_HLT = HLT.Ele8_CaloIdL_TrackIdL_IsoVL_PFJet30 or HLT.Ele12_CaloIdL_TrackIdL_IsoVL_PFJet30 or HLT.Ele23_CaloIdL_TrackIdL_IsoVL_PFJet30 or HLT.Mu8_TrkIsoVVL or HLT.Mu17_TrkIsoVVL or HLT.Mu15_IsoVVVL_PFHT600
-            #elif self.trig == "Tau":
-                #good_HLT = HLT.IsoMu20_eta2p1_LooseChargedIsoPFTau27_eta2p1_CrossL1 or HLT.Ele24_eta2p1_WPTight_Gsf_LooseChargedIsoPFTau30_eta2p1_CrossL1 or HLT.MediumChargedIsoPFTau50_Trk30_eta2p1_1pr
-            if self.trig == "HT":
-                good_HLT = HLT.PFHT250 or HLT.PFHT350 or HLT.PFHT370 or HLT.PFHT430 or HLT.PFHT510 or HLT.PFHT590 or HLT.PFHT680 or HLT.PFHT780 or HLT.PFHT890
+        else:
+            if("2016" in self.year):
+                if self.trig == "HT":
+                    good_HLT = HLT.PFHT125 or HLT.PFHT200 or HLT.PFHT250 or HLT.PFHT300 or HLT.PFHT350 or HLT.PFHT370 or HLT.PFHT430 or HLT.PFHT510 or HLT.PFHT590 or HLT.PFHT680 or HLT.PFHT780 or HLT.PFHT890
+            
+            elif("2017" in self.year):
+                if self.trig == "HT":
+                    good_HLT = HLT.PFHT180 or HLT.PFHT250 or HLT.PFHT350 or HLT.PFHT370 or HLT.PFHT430 or HLT.PFHT510 or HLT.PFHT590 or HLT.PFHT680 or HLT.PFHT780 or HLT.PFHT890
 
-        elif("2018" in self.year):
-            #if self.trig == "Lep":
-                #good_HLT = HLT.Ele8_CaloIdL_TrackIdL_IsoVL_PFJet30 or HLT.Ele12_CaloIdL_TrackIdL_IsoVL_PFJet30 or HLT.Ele23_CaloIdL_TrackIdL_IsoVL_PFJet30 or HLT.Mu8_TrkIsoVVL or HLT.Mu17_TrkIsoVVL or HLT.Mu15_IsoVVVL_PFHT600
-            #elif self.trig == "Tau":
-                #good_HLT = HLT.IsoMu20_eta2p1_LooseChargedIsoPFTau27_eta2p1_CrossL1 or HLT.Ele24_eta2p1_WPTight_Gsf_LooseChargedIsoPFTau30_eta2p1_CrossL1 or HLT.MediumChargedIsoPFTau50_Trk30_eta2p1_1pr
-            if self.trig == "HT":
-                good_HLT = HLT.PFHT250 or HLT.PFHT350 or HLT.PFHT370 or HLT.PFHT430 or HLT.PFHT510 or HLT.PFHT590 or HLT.PFHT680 or HLT.PFHT780 or HLT.PFHT890
-
+            elif("2018" in self.year):
+                if self.trig == "HT":
+                    good_HLT = HLT.PFHT180 or HLT.PFHT250 or HLT.PFHT350 or HLT.PFHT370 or HLT.PFHT430 or HLT.PFHT510 or HLT.PFHT590 or HLT.PFHT680 or HLT.PFHT780 or HLT.PFHT890
         else:
             print "Please specify the year: possible choices are 2016, 2017 or 2018"
         return good_MET and good_HLT
 
 MET_HLT_Filter_Fake_2016_HT = lambda : MET_HLT_Filter_Fake("2016", "HT")
+MET_HLT_Filter_Fake_UL2016_HT = lambda : MET_HLT_Filter_Fake("UL2016", "HT")
+MET_HLT_Filter_Fake_UL2016APV_HT = lambda : MET_HLT_Filter_Fake("UL2016APV", "HT")
 #MET_HLT_Filter_Fake_2017_Lep = lambda : MET_HLT_Filter_Fake("2017", "Lep")
 #MET_HLT_Filter_Fake_2017_Tau = lambda : MET_HLT_Filter_Fake("2017", "Tau")
 MET_HLT_Filter_Fake_2017_HT = lambda : MET_HLT_Filter_Fake("2017", "HT")
+MET_HLT_Filter_Fake_UL2017_HT = lambda : MET_HLT_Filter_Fake("UL2017", "HT")
 #MET_HLT_Filter_Fake_2018_Lep = lambda : MET_HLT_Filter_Fake("2018", "Lep")
 #MET_HLT_Filter_Fake_2018_Tau = lambda : MET_HLT_Filter_Fake("2018", "Tau")
+kMET_HLT_Filter_Fake_2017_HT = lambda : MET_HLT_Filter_Fake("2017", "HT")
 MET_HLT_Filter_Fake_2018_HT = lambda : MET_HLT_Filter_Fake("2018", "HT")
-
+MET_HLT_Filter_Fake_UL2018_HT = lambda : MET_HLT_Filter_Fake("UL2018", "HT")
