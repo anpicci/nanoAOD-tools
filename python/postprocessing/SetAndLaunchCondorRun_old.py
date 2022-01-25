c54f6d8017f33065e07b104b362bcd4feddf719f
apiccine <andrea.piccinelli@cern.ch>
2022-01-25 14:01:26 +0100

    temporary update

--- python/postprocessing/SetAndLaunchCondorRun_old.py
+++ python/postprocessing/SetAndLaunchCondorRun_old.py
@@ -146,8 +146,8 @@ else:
         opt.channel = 'bjet'
 
 print(opt.fold, opt.channel, folder)
-#path = "/eos/home-" + inituser + "/" + username + "/VBS/nosynch/" + folder + "/"
-path = "/eos/home-a/apiccine/VBS/nosynch/" + folder + "/"
+path = "/eos/home-" + inituser + "/" + username + "/VBS/nosynch/" + folder + "/"
+#path = "/eos/home-a/apiccine/VBS/nosynch/" + folder + "/"
 print("output path:", path, "\n")
 
 subpy = ""
@@ -214,19 +214,15 @@ for prname, proc in condor_dict.items():
                 if not (str(sample.label).startswith(opt.dat) or prname.startswith(opt.dat)):
                     continue
             
-            print("first check passed")
-
             if not DoesSampleExist(sample.name):
-                print("Crab file not found")
                 continue
                 #if sample.label in dirlist:
             if os.path.exists(path+sample.label):
                 if opt.rw:
                     print('Relaunching all the jobs for', sample.label)
                     os.system("rm -r "+ path + sample.label + "/*")
-            print("second check passed")
+
             if not AreAllCondored(sample.name, sample.label):
-                print("in AreAllCondored loop")
                 if opt.check:
                     print(sample.label, "not completely condored")
                     print("python " + subpy + " -d " + sample.label+ " " + optstring)
--- python/postprocessing/submit_condor_old.py
+++ python/postprocessing/submit_condor_old.py
@@ -72,8 +72,8 @@ def sub_writer(sample, n, files, folder):
     tagyear = str(sample.year)#.replace("UL", "").replace("APV", "")
     print(tagyear)
     f.write("transfer_input_files    = $(Proxy_path), samples/samples.py, samples/samplesUL.py, skimtree_utils_ssWW_wFakes_old.py, CutsAndValues.py, FR_vsjet2_" + tagyear + ".root, FR_vsjet4_" + tagyear + ".root, ./data/leptonSF/Muon_RunBCDEF_SF_ID_2017.root, TauIDSFTool.py, EFTOperator_dict.py, Btag_eff_" + tagyear + ".root, __init__.py, ./data\n")
-    #f.write("transfer_output_remaps  = \""+ sample.label + "_part" + str(n) + ".root=/eos/home-"+inituser + "/" + username+"/VBS/nosynch/" + folder + "/" + sample.label +"/"+ sample.label + "_part" + str(n) + ".root\"\n")
-    f.write("transfer_output_remaps  = \""+ sample.label + "_part" + str(n) + ".root=/eos/home-a/apiccine/VBS/nosynch/" + folder + "/" + sample.label +"/"+ sample.label + "_part" + str(n) + ".root\"\n")
+    f.write("transfer_output_remaps  = \""+ sample.label + "_part" + str(n) + ".root=/eos/home-"+inituser + "/" + username+"/VBS/nosynch/" + folder + "/" + sample.label +"/"+ sample.label + "_part" + str(n) + ".root\"\n")
+    #f.write("transfer_output_remaps  = \""+ sample.label + "_part" + str(n) + ".root=/eos/home-a/apiccine/VBS/nosynch/" + folder + "/" + sample.label +"/"+ sample.label + "_part" + str(n) + ".root\"\n")
     f.write("+JobFlavour             = \"nextweek\"\n") # options are espresso = 20 minutes, microcentury = 1 hour, longlunch = 2 hours, workday = 8 hours, tomorrow = 1 day, testmatch = 3 days, nextweek     = 1 week
     #args += "\n"
     args = sample.label + " " + str(n) + " " + str(files) + " remote " + opt.wpjet + " " + opt.wpele + " " + opt.wpmu
