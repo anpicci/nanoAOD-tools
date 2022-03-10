a23bc1a5c9cc085d3825922b09dee5c561699982
apiccine <andrea.piccinelli@cern.ch>
2022-03-10 17:24:03 +0100

    test bug update

--- python/postprocessing/submit_condor_old.py
+++ python/postprocessing/submit_condor_old.py
@@ -62,6 +62,11 @@ def sub_writer(sample, n, files, folder):
     condorsubb = condorsub + "_" + str(sample.year) + ".sub"
     outputpath = "/eos/home-a/apiccine/VBS/nosynch/" + folder + "/" + sample.label +"/"
     print(condorsub, condorsubb)
+    runtype = ""
+    if fsplitted[0] == "vUL001":
+        runtype = "test"
+    else:
+        runtype = "remote"
     f = open(condorsubb, "w")
     f.write("Proxy_filename          = x509up\n")
     f.write("Proxy_path              = /afs/cern.ch/user/" + inituser + "/" + username + "/private/$(Proxy_filename)\n")
@@ -72,16 +77,15 @@ def sub_writer(sample, n, files, folder):
     f.write("when_to_transfer_output = ON_EXIT\n")
     tagyear = str(sample.year)#.replace("UL", "").replace("APV", "")
     print(tagyear)
-    f.write("transfer_input_files    = $(Proxy_path), samples/samples.py, samples/samplesUL.py, skimtree_utils_ssWW_wFakes_old.py, CutsAndValues.py, FR_vsjet2_" + tagyear + ".root, FR_vsjet4_" + tagyear + ".root, FR_vsjet8_" + tagyear + ".root, ./data/leptonSF/Muon_RunBCDEF_SF_ID_2017.root, TauIDSFTool.py, EFTOperator_dict.py, Btag_eff_" + tagyear + ".root, __init__.py, ./data\n")
+    inputfiles = "transfer_input_files    = $(Proxy_path), samples/samples.py, samples/samplesUL.py, skimtree_utils_ssWW_wFakes_old.py, CutsAndValues.py, FR_vsjet2_" + tagyear + ".root, FR_vsjet4_" + tagyear + ".root, FR_vsjet8_" + tagyear
+    if runtype == "test":
+        inputfiles += "_test"
+    inputfiles += ".root, ./data/leptonSF/Muon_RunBCDEF_SF_ID_2017.root, TauIDSFTool.py, EFTOperator_dict.py, Btag_eff_" + tagyear + ".root, __init__.py, ./data\n"
+    f.write(inputfiles)
     #f.write("transfer_output_remaps  = \""+ sample.label + "_part" + str(n) + ".root=/eos/home-"+inituser + "/" + username+"/VBS/nosynch/" + folder + "/" + sample.label +"/"+ sample.label + "_part" + str(n) + ".root\"\n")
     #f.write("transfer_output_remaps  = \""+ sample.label + "_part" + str(n) + ".root=/eos/home-a/apiccine/VBS/nosynch/" + folder + "/" + sample.label +"/"+ sample.label + "_part" + str(n) + ".root\"\n")
     f.write("+JobFlavour             = \"nextweek\"\n") # options are espresso = 20 minutes, microcentury = 1 hour, longlunch = 2 hours, workday = 8 hours, tomorrow = 1 day, testmatch = 3 days, nextweek     = 1 week
     #args += "\n"
-    runtype = ""
-    if fsplitted[0] == "vUL001":
-        runtype = "test"
-    else:
-        runtype = "remote"
     args = sample.label + " " + str(n) + " " + str(files) + " " + runtype + " " + opt.wpjet + " " + opt.wpele + " " + opt.wpmu
 
     if opt.masscr:
