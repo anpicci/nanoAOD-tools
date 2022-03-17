6b6d43507d9e388a469f37671ba264f13adffbc1
pinkDreud <matteo.magherini@gmail.com>
2022-03-17 11:24:31 +0100

    countings ouput to csv

--- python/postprocessing/makeplot.py
+++ python/postprocessing/makeplot.py
@@ -1616,6 +1616,7 @@ for year in years:
         '''
         #variables.append(variabile('event_RT', 'R_{T}',  wzero+'*('+cutbase+')', 30, 0., 3.))
 
+
         for sample in dataset_new:
             print(sample.label, sample.name)
             if ('DataHT' in sample.label or 'DataMET' in sample.label) and not opt.folder.startswith("CTHT"):# or "WJets" in sample.label:
@@ -1631,8 +1632,9 @@ for year in years:
                                 os.makedirs(pathplot + 'countings/')
                             if not os.path.exists(pathplot + 'countings/' + cut_tag):
                                 os.makedirs(pathplot + 'countings/' + cut_tag)
-                            if not os.path.exists(pathplot + 'countings/' + cut_tag + "/" + var._name + "_" + str(opt.year) + ".txt"):
-                                tmp_f = open(pathplot + 'countings/' + cut_tag + "/" + var._name + "_" + str(opt.year) + ".txt", "w")
+                            if not os.path.exists(pathplot + 'countings/' + cut_tag + "/" + var._name + "_" + str(opt.year) + ".csv"):
+                                tmp_f = open(pathplot + 'countings/' + cut_tag + "/" + var._name + "_" + str(opt.year) + ".csv", "w")
+                                tmp_f.write("Process,yields,error\n")
                                 tmp_f.close()
                         if (("GenPart" in var._name) or ("MC_" in var._name)) and "Data" in sample.label:
                             continue
