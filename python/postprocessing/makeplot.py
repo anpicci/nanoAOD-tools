73bec89ca00764d03f3c33dd3efa4f2a0c47d239
apiccine <andrea.piccinelli@cern.ch>
2022-05-10 22:03:20 +0200

    2016M added in makeplot for stacks

--- python/postprocessing/makeplot.py
+++ python/postprocessing/makeplot.py
@@ -1042,7 +1042,7 @@ leptons = opt.lep.split(',')
 if not "UL" in opt.year:
     dataset_dict = {'2017':[],'2018':[]}
 else:
-    dataset_dict = {'UL2016APV':[], 'UL2016': [], 'UL2017':[], 'UL2018':[]}
+    dataset_dict = {'UL2016APV':[], 'UL2016': [], 'UL2016M':[], 'UL2017':[], 'UL2018':[]}
 #print(class_list)
 
 if(opt.dat != 'all'):
